c98417862856020ccee7e4ad593d7d2b42e0df5b
Marcelo <pbsmarcelo@gmail.com>
2022-11-15 15:15:58 -0300

    Voltando para o estado anterior

--- app.js
+++ app.js
@@ -2,5 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação se conectando ao banco de dados")
 //Mais um console log de teste:
 console.log("Será feito um git pull local para ver estas alterações")
-
-console.log("Enviando novos dados para o Github")
