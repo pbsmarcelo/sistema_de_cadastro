1c2321e9b053ad28dccdf612056f14151e8f759f
Marcelo <pbsmarcelo@gmail.com>
2022-11-15 15:01:13 -0300

    New consolelog in app.js

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação se conectando ao banco de dados")
 //Mais um console log de teste:
 console.log("Será feito um git pull local para ver estas alterações")
+
+console.log("Enviando novos dados para o Github")
