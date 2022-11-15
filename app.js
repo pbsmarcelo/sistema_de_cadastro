307bb58db638e74a993a00d7776b437c7a337367
Marcelo PB Silva <69975240+pbsmarcelo@users.noreply.github.com>
2022-11-15 14:52:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicação se conectando ao banco de dados")
+//Mais um console log de teste:
+console.log("Será feito um git pull local para ver estas alterações")
